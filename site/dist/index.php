316a2ca9c16628af365934c6ab1ac29a0c93e131
sridharbharadwaj <91248129+sridharbharadwaj@users.noreply.github.com>
2025-02-17 12:01:08 +0800

    Commit

--- site/dist/index.php
+++ site/dist/index.php
@@ -85,7 +85,7 @@
 
     <head>
         <meta charset="utf-8" />
-        <title>Home</title>
+        <title>Home-New</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
         <meta content="Themesdesign" name="author" />
